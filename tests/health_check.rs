4276608b0d19817c8547d0d19eaa51430bc354cd
Luiz Carlos Martins Filho <lcmf@pm.me>
2022-11-10 20:52:32 -0300

    wip - prepare for test.

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,6 +1,17 @@
-use zero2prod::main;
+#[tokio::test]
+async fn health_check_works() {
+    spawn_app().await.expect("Failed to spawn our app.");
+    let client = reqwest::Client::new();
+    let response = client
+            .get("http://127.0.0.1:8000/health_check")
+            .send()
+            .await
+            .expect("Failed to execute request."); 
 
-#[test]
-fn dummy_test() {
-    main()
+    assert!(response.status().is_success());
+    assert_eq!(Some(0), response.content_length());
+}
+
+async fn spawn_app() -> std::io::Result<()> {
+    unimplemented!()
 }
