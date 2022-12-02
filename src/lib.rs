984741713ea3b99cecf33e6681adaed69700af54
Luiz Carlos Martins Filho <lcmf@pm.me>
2022-12-02 18:39:59 -0300

    wip - add first meaningful route.

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,28 @@
 use actix_web::dev::Server;
-use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{web, App, HttpResponse, HttpServer};
 use std::net::TcpListener;
 
-async fn health_check() -> impl Responder {
-    HttpResponse::Ok()
+// We were returning `impl Responder` at the very beginning.
+// We are now spelling out the type explicitly given that we have
+// become more familiar with `actix-web`.
+// There is no performance difference! Just a stylistic choice! :)
+// Before: async fn health_check() -> impl Responder
+// Remember: Responder is a trait implemented by types that can be
+// converted to HttpResponse
+async fn health_check() -> HttpResponse {
+    HttpResponse::Ok().finish()
+}
+
+async fn subscribe() -> HttpResponse {
+    HttpResponse::Ok().finish()
 }
 
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
     let server = HttpServer::new(|| {
-        App::new().route("/health_check", web::get().to(health_check))
+        App::new()
+            .route("/health_check", web::get().to(health_check))
+            // A new entry in our routing table for POST /subscriptions requests
+            .route("/subscriptions", web::post().to(subscribe))
     })
         .listen(listener)?
         .run();
